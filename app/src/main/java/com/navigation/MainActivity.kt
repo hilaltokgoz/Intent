5b1e367b64d7d789173456c84c1bbfaf3197c29c
Hilal <hilaltokgoz98@gmail.com>
2022-02-02 16:37:52 +0300

    login finish commit

--- app/src/main/java/com/navigation/MainActivity.kt
+++ app/src/main/java/com/navigation/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.navigation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.navigation.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //binding : activity xml dosyamızda bulunan elementlere güvenli bir şekilde erişimini sağlar.
+        binding.button.setOnClickListener {
+        val intent= Intent(this,SecondActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
